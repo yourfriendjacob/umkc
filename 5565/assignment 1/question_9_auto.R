ae49084345683b56beccbc4867d223b164d07976
Jacob Sauther <jacobsauther@gmail.com>
2022-09-03 11:07:33 -0500

    Finished question 9

--- 5565/assignment 1/question_9_auto.R
+++ 5565/assignment 1/question_9_auto.R
@@ -48,9 +48,55 @@ yar_sd = sd(auto_data$year)               # sd ~3.684
 hrp_sd = sd(auto_data$horsepower)         # sd ~38.450
 
 
-# TODO 9.d needs to be done
-
+#range
 apply(auto_data[-(10:85), 1:7], MARGIN = 2, FUN = "range")
+# mpg          11.0 - 46.6
+# cylinders    3 - 8
+# displacement 68 - 455
+# horsepower   46 - 230
+# weight       1649 - 4997
+# acceleration 8.5 - 24.8
+# year         70 - 82
 
+#mean
+apply(auto_data[-(10:85), 1:7], MARGIN = 2, FUN = "mean")
+# mpg          24.404
+# cylinders    5.373
+# displacement 187.241
+# horsepower   100.722
+# weight       2935.972
+# acceleration 15.727
+# year         77.146
 
+#standard deviation
+apply(auto_data[-(10:85), 1:7], MARGIN = 2, FUN = "sd")
+# mpg          7.867
+# cylinders    1.654
+# displacement 99.678
+# horsepower   35.709
+# weight       811.300
+# acceleration 2.694
+# year         3.106
+
+par(mfrow = c(2, 4))
+plot(auto_data$mpg, auto_data$weight)
+plot(auto_data$mpg, auto_data$cylinders)
+plot(auto_data$acceleration, auto_data$horsepower)
+plot(auto_data$acceleration, auto_data$cylinders)
+plot(auto_data$mpg, auto_data$year)
+plot(auto_data$horsepower, auto_data$year)
+plot(auto_data$horsepower, auto_data$displacement)
+plot(auto_data$mpg, auto_data$horsepower)
+# it looks like mpg improves as a car gets lighter. The number of cylinders also seem to make an impact too.
+# I see similar results with mpg on horsepower.
+# it is kinda interesting to see that more horsepower appears to indicate less acceleration
+# better acceleration appears to co-related with less cylinders
+# higher displacement appears to mean higher horsepower
+
+#
+pairs(auto_data)
+
+#Looking at the pairs for the data I think cylinders, displacement, horsepower, and weight. Could be used to predict the mpg of a car.
+#It appears between all the predictors these seem to have a high impact on the mpg. Acceleration may also help, but judging from its relationship with
+# horsepower it would seem that you would not need to have both to make a prediction on mpg.
 
